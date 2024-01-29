218341100ef985570349850d0298b70ce3a215ae
Ingrid Coronel <ingridcoronelc@gmail.com>
2024-01-29 15:35:41 -0600

    First Commit

---
+++ modules/sns/mai.tf
@@ -0,0 +1,13 @@
+resource "aws_sns_topic" "sns_topic" {
+  name = "topic-test-full-tf"
+}
+
+resource "aws_sns_topic_subscription" "sns_subs_email" {
+  topic_arn = aws_sns_topic.sns_topic.arn
+  protocol  = "email"
+  endpoint  = "ingridcoronelc@gmail.com"
+
+  depends_on = [
+    aws_sns_topic.sns_topic
+  ]  
+}
